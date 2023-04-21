f17a25f45f3e8796805e5ef936ec6433c807b417
franxltr <marulandaf585@gmail.com>
2023-04-21 13:07:56 -0500

    verdolaga

--- src/Presentaciones.js
+++ src/Presentaciones.js
@@ -1,10 +1,13 @@
-
-function Presentaciones() {
-  return (
+import Datosfran from "./componentes/Fran/Datosfran";
+function Presentaciones (){
+   return (
     <div >
         <h1>Estudiantes del Curso</h1>
+        <Datosfran/>
     </div>
   );
+
 }
+ 
 
 export default Presentaciones;
--- src/componentes/Fran/Datosfran.js
+++ src/componentes/Fran/Datosfran.js
@@ -1,5 +1,12 @@
-function componentes() {
-    return (
-      cd  
-    )
-  }
+import './Datosfran.css'
+
+function Datosfran() {
+  return (
+    <div className="fran">
+      <h1 className='titulo'>hablame caramel</h1>
+      <p>Hola me gusta mucho no hacer nada :D</p>
+    </div>
+  );
+}
+
+export default Datosfran;
