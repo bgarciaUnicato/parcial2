0ddcc441ede31a19f4e0de75379b7f3077a39175
juansanchez1023 <juansanchez06@unicatolica.edu.co>
2023-04-21 11:37:04 -0500

    Sebas

---
+++ src/componentes/Sebastian/Datossebastian.js
@@ -0,0 +1,13 @@
+function Datossebastian() {
+    return (
+      <div >
+        
+          <h1>Mi Biografia</h1>
+          <p>Mi nombre es Sebastian Gutierrez</p>
+          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRcis_2kB4jyN0Tox5xiOXXVgDk4Atklpx9fg&usqp=CAU"></img>
+          <p>Tengo 18 años, naci en la ciudad Santiago de Cali el 19 de diciembre del 2004, soy una persona alegre de ambiente, </p>
+      </div>
+    );
+  }
+  
+  export default Datossebastian;
