b0cfeef63582e76cf2ec78c374a01dd7856f8840
SamuelPacheco03 <samuel2003pacheco@gmail.com>
2023-04-21 11:33:58 -0500

    Sam

---
+++ src/Presentaciones.js
@@ -0,0 +1,12 @@
+import Datossamuel from "./componentes/Samuel/Datossamuel";
+
+function Presentaciones() {
+  return (
+    <div >
+        <h1>Estudiantes del Curso</h1>
+        <Datossamuel />
+    </div>
+  );
+}
+
+export default Presentaciones;
---
+++ src/componentes/Samuel/Datossamuel.js
@@ -0,0 +1,20 @@
+<link href="C:\Users\Samuel Pacheco\Desktop\Parcial2\parcial2-main\src\componentes\imagen.css" rel="stylesheet"></link>
+
+function Datossamuel() {
+    return (
+
+      
+    
+
+      <div >
+      
+
+          <h1>Biografia de Samuel</h1>
+
+          <p>Mi nombre es Samuel, tengo 19 años y actualmente estoy estudiando ingenieria de sistemas en unicatolica</p>
+          <img class="imagenes" src="https://scontent-bog1-1.xx.fbcdn.net/v/t39.30808-6/342057928_887173242575751_60018635499548725_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeGFNae2wsYwpsvcVG6uiKzubxYEZBUN445vFgRkFQ3jjtc6OHDjyl3uEdJjBkir2kG7R3XqUEZ3mIMQqx3B65rk&_nc_ohc=CmudNvPNV2sAX8DSatw&_nc_ht=scontent-bog1-1.xx&oh=00_AfARdRtt_Mz4QzHEbnmbjifLZUKNN7l5q7-oT2wP0c5ChQ&oe=6447A656" />
+      </div>
+    );
+  }
+  
+  export default Datossamuel;
