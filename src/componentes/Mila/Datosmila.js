712c1fdcc0a9764a4e948c628e8f1f18159e90e5
Milaespi <maria.espinosa01@unicatolica.edu.co>
2023-04-21 11:47:19 -0500

    firts commit

--- src/componentes/Mila/Datosmila.js
+++ src/componentes/Mila/Datosmila.js
@@ -2,7 +2,9 @@
 function Datosmila() {
     return (
       <div >
-          <h1>Maria Camila Espinosa </h1>
+          <h1>Maria Camila Espin </h1>
+          <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQIVF3-1mYojYcromuZ7yq4MUkK8-3nlJ98xuPAubSX&s' width={200} height={200} alt='img1'/>
+
       </div>
     );
   }
