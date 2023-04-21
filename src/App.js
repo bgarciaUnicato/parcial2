6e632524a4d46d285f28dfde8c545837717c2d47
franxltr <marulandaf585@gmail.com>
2023-04-21 11:36:15 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Presentaciones from './Presentaciones';
+
+function App() {
+  return (
+    <div className="App">
+      <Presentaciones />
+    </div>
+  );
+}
+
+
+export default App;
