1885337f045e06784d3c4ed704a1bee3d6a3f654
fckpipe <juan.henao03@unicatolica.edu.co>
2023-04-21 13:07:59 -0500

    mechita

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Presentaciones from './Presentaciones';
 
+
 function App() {
   return (
     <div className="App">
--- src/Presentaciones.js
+++ src/Presentaciones.js
@@ -1,8 +1,9 @@
-
+import Datospipe from "./componentes/pipe/Datospipe";
 function Presentaciones() {
   return (
     <div >
         <h1>Estudiantes del Curso</h1>
+        <Datospipe/>
     </div>
   );
 }
