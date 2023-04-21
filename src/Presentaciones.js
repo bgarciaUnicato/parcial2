20cc0808bad586975df35c947e039a74527ca4eb
Milaespi <maria.espinosa01@unicatolica.edu.co>
2023-04-21 11:34:05 -0500

    firts commit

---
+++ src/Presentaciones.js
@@ -0,0 +1,11 @@
+import Datosmila from "./componentes/Mila/Datosmila";
+function Presentaciones() {
+  return (
+    <div >
+        <h1>Estudiantes del Curso</h1>
+        <Datosmila />
+    </div>
+  );
+}
+
+export default Presentaciones;
---
+++ src/componentes/Mila/Datosmila.js
@@ -0,0 +1,10 @@
+
+function Datosmila() {
+    return (
+      <div >
+          <h1>Maria Camila Espinosa </h1>
+      </div>
+    );
+  }
+  
+  export default Datosmila;
