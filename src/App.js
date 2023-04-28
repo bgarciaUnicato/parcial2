374c1c092f96c951282c5aa444d1819b2158af6f
Milaespi <maria.espinosa01@unicatolica.edu.co>
2023-04-28 12:20:31 -0500

    push 3

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Presentaciones from './Presentaciones';
+import Datosmila from'./componentes/Mila/Datosmila.js';
 
 function App() {
   return (
     <div className="App">
       <Presentaciones />
+      <Datosmila></Datosmila>
     </div>
   );
 }
--- src/componentes/Mila/Datosmila.js
+++ src/componentes/Mila/Datosmila.js
@@ -1,15 +1,25 @@
 import imagen from "./img1.jpeg"
+import imagen2 from "./img2.jpeg"
 import './imagen1.css';
+import './imagen2.css';
+
 
 
 function Datosmila() {
     return (
       <div >
           <h1>Maria Camila Espinosa Florez</h1>
+          <br></br>
           <img class="imagen1" src={imagen}alt='img1'/>
-          <p> Me llamo Maria Camila pero me l</p>
-
+          
+          <p> Me llamo Maria Camila pero me llaman Mila, naci el 10/10/2002, osea tengo 20 años, estudio ingenieria de sistemas y estoy en 4 semestre en la Unicatolica. Primero era de ingenieria industrial, pero en ya en 3 semestre me cambie para sistemas y me encuentro feliz</p>
+          <br></br>
+          <p> Me gusta dormir, bailar y escuhar musica, pero no me gusta la cebolla, el coco y esperar, aunque tengo muchas mas cosas que no me gustan como que me cogan las coasas sin permiso y que cuenten con mi tiempo pero tambien tengo mas cosas que me gustan como la salchipapa, pintar y ver doramas</p>
+          <br></br>
+          <p> Tengo 2 gatos que son mi adoracion, uno se llama Jacob que es el mayor que es un amor, super consentido y no molesta, y el otro se llama Alan que es el menor que ese si es un diablito, siempre daña cosas  y le encanta molestar al hermano pero los amo igual  </p>
+          <img class="imagen2" src={imagen2}alt='img2'/>
       </div>
+    
     );
   }
 
