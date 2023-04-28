1ed6918898b8cd73421e97c02b2c1973bf15cecf
JuanCamiloGrajales <Juan.grajales02@unicatolica.edu.co>
2023-04-28 11:11:15 -0500

    first commit

---
+++ src/componentes/JuanCamiloGrajales/Datosjcamilo.js
@@ -0,0 +1,25 @@
+import FotoCamilo from './componentes/JuanCamiloGrajales/FotoCamilo.jpg';
+
+function Datosjcamilo() {
+    return (
+      <div>
+        
+        <h2> Juan Camilo Grajales </h2>
+          
+        <img src={FotoCamilo} alt="Foto Camilo" style="float:left;  margin-right:10px; width:100px; height:100px;" />
+
+          <p>
+           
+          Soy Juan Camilo Grajales Lasso, nací el 11 de abril de 2003 en la ciudad de Cali, Colombia. Me gradué de los colegios Comfandi Miraflores y Santa Isabel de Hungría sede Ciudad 2000. Actualmente sigo viviendo con mi familia en Cali y estoy realizando un pregrado de ingeniería de sistemas en la universidad católica Lumen Gentum. Mis pasatiempos durante mi tiempo libre son jugar videojuegos, escuchar música y ver películas.
+        
+          </p>
+          
+
+
+        
+        
+      </div>
+    );
+  }
+
+  export default Datosjcamilo;  
