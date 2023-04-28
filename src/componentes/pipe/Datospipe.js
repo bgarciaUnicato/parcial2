f70f08f55554d5e71a2ff2df1d921c96f4d9b392
fckpipe <juan.henao03@unicatolica.edu.co>
2023-04-28 11:36:02 -0500

    parcial

--- src/componentes/pipe/Datospipe.js
+++ src/componentes/pipe/Datospipe.js
@@ -4,7 +4,7 @@ function Datospipe() {
     return (
         <div>
             <h1 className='bio'>Felipe Henao Hoyos</h1>
-            <p className='biotext'>Lorem ipsum dolor sit amet. Aut voluptas quia rem ducimus enim ut doloremque maiores aut rerum rerum sed internos atque. Aut dolores consequatur et atque maxime et vitae voluptatem! </p>
+            <p className='biotext'>Lorem ipsum . Aut voluptas quia rem ducimus enim ut doloremque maiores aut rerum rerum sed internos atque. Aut dolores consequatur et atque maxime et vitae voluptatem! </p>
             <img src='./img1.png' className='imagen1'/>
             
         </div>
