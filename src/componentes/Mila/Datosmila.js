43a6b5f1e8838468f8f58da5583042df0519d3e7
Milaespi <maria.espinosa01@unicatolica.edu.co>
2023-04-28 11:23:06 -0500

    push 2

--- src/componentes/Mila/Datosmila.js
+++ src/componentes/Mila/Datosmila.js
@@ -5,8 +5,9 @@ import './imagen1.css';
 function Datosmila() {
     return (
       <div >
-          <h1>Maria Camila Espinosa Flore </h1>
+          <h1>Maria Camila Espinosa Florez</h1>
           <img class="imagen1" src={imagen}alt='img1'/>
+          <p> Me llamo Maria Camila pero me l</p>
 
       </div>
     );
