f69c0a37ef81f1ee5aa92a7947314deec42476ab
JuanesRivera25 <129534701+JuanesRivera25@users.noreply.github.com>
2023-04-28 11:35:39 -0500

    Segundo parcial

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
 import './App.css';
 import Presentaciones from './Presentaciones';
-<<<<<<< HEAD
-=======
-import Juanes from './componentes/juanesrivera/Juanes';
->>>>>>> 8aac0f1550974b0391d753c8289d1d2930d78268
+import Juanes from './componentes/juanes/Juanes';
+
 
 function App() {
   return (
     <div className="App">
       <Presentaciones />
-<<<<<<< HEAD
-=======
       <Juanes />
->>>>>>> 8aac0f1550974b0391d753c8289d1d2930d78268
     </div>
   );
 }
 
-<<<<<<< HEAD
-
-=======
->>>>>>> 8aac0f1550974b0391d753c8289d1d2930d78268
 export default App;
