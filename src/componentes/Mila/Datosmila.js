7fb2655ca545f834fd5850d7f892d1ddf1071dd4
Milaespi <maria.espinosa01@unicatolica.edu.co>
2023-04-28 11:13:55 -0500

    primer merge

--- src/componentes/Mila/Datosmila.js
+++ src/componentes/Mila/Datosmila.js
@@ -5,7 +5,7 @@ import './imagen1.css';
 function Datosmila() {
     return (
       <div >
-          <h1>Maria Camila Espinosa Florez </h1>
+          <h1>Maria Camila Espinosa Flore </h1>
           <img class="imagen1" src={imagen}alt='img1'/>
 
       </div>
