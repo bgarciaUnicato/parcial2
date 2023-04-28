0399f9b11ec7e8cd2c50452f7fb24d27e487e5e5
Juan Esteban Rivera <juan.rivera03@unicatolica.edu.co>
2023-04-27 22:49:05 -0500

    Cambios

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import './App.css';
 import Presentaciones from './Presentaciones';
+import Juanes from './componentes/juanesrivera/Juanes';
 
 function App() {
   return (
     <div className="App">
       <Presentaciones />
+      <Juanes />
     </div>
   );
 }
-
 
 export default App;
