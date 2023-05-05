e6a495d4a8a2805ad189cbbd717b55d501934f72
Milaespi <maria.espinosa01@unicatolica.edu.co>
2023-05-05 12:50:33 -0500

    push 6

--- src/Presentaciones.js
+++ src/Presentaciones.js
@@ -1,22 +1,10 @@
-function Presentaciones() {
-  return (
-    <div >
-        <h1>Estudiantes del Curso</h1>
-    </div>
-  );
-}
-
-
 export default Presentaciones;
-import Datossebastian from "./componentes/Sebastian/Datossebastian";
 function Presentaciones() {
   return (
     <div >
-        <h1>Estudiantes del Curso</h1>
-        <Datossebastian />
+         
     </div>
     
   );
 }
 
-export default Presentaciones;
--- src/componentes/Mila/Datosmila.js
+++ src/componentes/Mila/Datosmila.js
@@ -1,23 +1,17 @@
-import imagen from "./img1.jpeg"
-import imagen2 from "./img2.jpeg"
+import imagen from "./img1.jpg"
 import './imagen1.css';
-import './imagen2.css';
-
 
 
 function Datosmila() {
     return (
       <div >
-          <h1>Maria Camila Espinosa Florez</h1>
+          <h1>GARFIELD</h1>
           <br></br>
           <img class="imagen1" src={imagen}alt='img1'/>
              
-          <p> Me llamo Maria Camila pero me llaman Mila , naci el 10/10/2002, osea tengo 20 años, estudio ingenieria de sistemas y estoy en 4 semestre en la Unicatolica. Primero era de ingenieria industrial, pero en ya en 3 semestre me cambie para sistemas y me encuentro feliz</p>
-          <br></br>
-          <p> Me gusta dormir, bailar y escuhar musica, pero no me gusta la cebolla, el coco y esperar, aunque tengo muchas mas cosas que no me gustan como que me cogan las coasas sin permiso y que cuenten con mi tiempo pero tambien tengo mas cosas que me gustan como la salchipapa, pintar y ver doramas</p>
-          <br></br>
-          <p> Tengo 2 gatos que son mi adoracion, uno se llama Jacob que es el mayor que es un amor, super consentido y no molesta, y el otro se llama Alan que es el menor que ese si es un diablito, siempre daña cosas  y le encanta molestar al hermano pero los amo igual  </p>
-          <img class="imagen2" src={imagen2}alt='img2'/>
+          <p> Garfield y sus amigos fue una serie animada estadounidense, producida por DPS Film Roman, fue basada en la popular tira cómica Garfield, creada por Jim Davis. La serie fue transmitida por la cadena de televisión CBS en las mañanas de los sábados, desde el 17 de septiembre de 1988 hasta el 18 de febrero de 1995 en Estados Unidos.</p>
+    
+        
       </div>
     
     );
