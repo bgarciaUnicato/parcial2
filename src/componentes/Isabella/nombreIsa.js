707fb6ee0fa4ba4cd8bd1535213ae81818165392
Dead5tar <jhonalexander90@gmail.com>
2023-05-05 12:13:36 -0500

    new commit

---
+++ src/componentes/Isabella/nombreIsa.js
@@ -0,0 +1,39 @@
+import tituloIsa from "./titulo.jpg"
+import imgnombres from "./imgnombres.jpg"
+import './cositas.css';
+import divisionisa from "./divisionisa.png"
+import personajesIsa from './personajesIsa';
+import episodiosIsa from './episodiosIsa';
+
+function nombreisa() {
+  return (
+    <div className="App">
+      <personajesIsa />
+      <episodiosIsa/>
+
+      <div class="container text-center">
+
+       <div class="row align-items-start">
+         <img className="tituloIsa" src={tituloIsa} /> 
+       </div>
+
+       <div class="row align-items-start">
+         <img className="imgnombres" src={imgnombres} />
+       </div>
+
+       <div class="row align-items-start">
+         <p class="pnombres">Rick y Morty es una serie de televisión americana de animación para adultos creada por Justin Roiland 
+          y Dan Harmon en 2013 para Adult Swim. La serie sigue las desventuras de un científico, Rick Sanchez, y su fácilmente influenciable nieto, 
+          Morty, quienes pasan el tiempo entre la vida doméstica y los viajes espaciales, temporales e intergalácticos. </p>
+       </div>
+
+       <img class="divisionisa" src={divisionisa} />
+
+       </div>
+ 
+
+    </div>
+  );
+}
+
+export default nombreisa;
