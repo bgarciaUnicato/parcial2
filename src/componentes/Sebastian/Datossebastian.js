22ca024ada9ad0dee6ef63387c56753b92d3d14e
XIDEMONSIX <demonsgutierrez962@gmail.com>
2023-06-02 11:47:23 -0500

    h

--- src/componentes/Sebastian/Datossebastian.js
+++ src/componentes/Sebastian/Datossebastian.js
@@ -1,51 +1,108 @@
 import "./imagen.css"
+import Container from "react-bootstrap/Container"
+import Row from "react-bootstrap/Row"
+import Col from "react-bootstrap/Col"
+
 function Datossebastian() {
     return (
-      <div >
-        
-        <div class="container text-left">
 
-         <div class="row">
+      <Container>
+        <h1 class="Titulos">The Simpsons</h1>
+  
+        <img class="imagen1" src="http://2.bp.blogspot.com/_XRHLx8HW7OQ/SpiHXtBpsBI/AAAAAAAAAAk/QuMM5-VU5wA/S1600-R/1558597Fondo.Simpsons.gif" alt="The Simpsons" />
+       <br></br>
+        <p class="ParrafoDescripcion">Los Simpson es una popular serie de televisión estadounidense de animación para adultos que se emite desde 1989. La serie gira en torno a la familia Simpson, compuesta por Homer, Marge, Bart, Lisa y Maggie, y sus aventuras en la ciudad ficticia de Springfield.
 
-           <div class="col-center">
-           <h1 class="Titulo_Sebastian">The Simpsons</h1>
-           
-          <img class="imagen" src="http://3.bp.blogspot.com/_VIQ3rOhI0IY/TAUDAstdPMI/AAAAAAAABq0/XGTvWgYEKDs/s1600/1558597fondosimpsons.gif"></img>
-          </div>
+Los personajes de Los Simpson son icónicos y han sidos reconocidos en todo el mundo. Homer es un padre de familia poco convencional, a menudo perezoso y aficionado a la cerveza. Marge es la esposa de Homer y una madre cariñosa, siempre tratando de mantener la paz en su hogar. Bart es el hijo mayor, un bromista rebelde con una personalidad traviesa. Lisa es la hija inteligente y sensible, apasionada por la música y la justicia social. Maggie es la bebé de la familia, que rara vez habla pero que es muy astuta.
 
-           <div class="col">
+La serie utiliza el humor satírico para abordar una amplia variedad de temas, incluyendo la política, la religión, la cultura pop, la familia y las relaciones interpersonales. Los personajes a menudo se enfrentan a situaciones absurdas y cómicas, y la serie ha sido el hogar de numerosos cameos de celebridades a lo largo de los años.</p>
+         <br></br>
+        <p class="Linea"></p>
         <br></br>
-           <p>Los Simpson es una popular serie de televisión estadounidense de animación para adultos que se emite desde 1989. La serie gira en torno a la familia Simpson, compuesta por Homer, Marge, Bart, Lisa y Maggie, y sus aventuras en la ciudad ficticia de Springfield.</p>
-            <p>Los personajes de Los Simpson son icónicos y han sido reconocidos en todo el mundo. Homer es un padre de familia poco convencional, a menudo perezoso y aficionado a la cerveza. Marge es la esposa de Homer y una madre cariñosa, siempre tratando de mantener la paz en su hogar. Bart es el hijo mayor, un bromista rebelde con una personalidad traviesa. Lisa es la hija inteligente y sensible, apasionada por la música y la justicia social. Maggie es la bebé de la familia, que rara vez habla pero que es muy astuta.</p>
-            <p>La serie utiliza el humor satírico para abordar una amplia variedad de temas, incluyendo la política, la religión, la cultura pop, la familia y las relaciones interpersonales. Los personajes a menudo se enfrentan a situaciones absurdas y cómicas, y la serie ha sido el hogar de numerosos cameos de celebridades a lo largo de los años.</p>
-            <div class="container text-center">
-             
-             <h2>PERSONAJES</h2> 
-            
-            <div class="container">
-             <div class="row">
-                <div class="col-1">
-                 <img class="imagen4" src="http://3.bp.blogspot.com/-IvHoRUU9Mqo/ULjIUJ2g5UI/AAAAAAAAABM/EvkrT6I3wQs/s1600/homero.jpg"></img>
-                </div>
-                <div class="col">
-                <p>Homer Simpson es el padre de la familia Simpson en la serie de televisión estadounidense "Los Simpson". Es un personaje icónico conocido por su personalidad perezosa, su amor por la cerveza y su tendencia a meterse en problemas debido a sus decisiones impulsivas. A menudo es torpe e incompetente, pero también puede ser cariñoso y protector con su familia. A pesar de sus defectos, es uno de los personajes más queridos de la serie y ha sido uno de los mayores atractivos de la misma desde su inicio en 1989.</p>
-                </div>
-              </div>
-            </div>
-           
+        
+        <h1 class="Titulos">Personajes</h1>
+        <br></br>
+        <br></br>
+        <Row>
+          <Col md={4}>  <img class="imagen2" src="https://tse2.mm.bing.net/th?id=OIP.gyK2-LW0xWFfTA9YnVjsPgHaHa&pid=Api&P=0&h=180" alt="The Simpsons" /> </Col>
+          <Col md={8}> <h2 class="Subtitulos"> HOMERO</h2> 
+          <p class="texto_Personaje">Homer Simpson es el patriarca de la familia Simpson en la popular serie de televisión "Los Simpson". Es un hombre de mediana edad, obeso y calvo, con una personalidad excéntrica y un sentido del humor peculiar. Homer trabaja como supervisor en la planta de energía nuclear de Springfield y es conocido por su pereza y falta de ambición.
 
-             <div>
-            
-            
-             </div>
+Homer es un amante de la cerveza y pasa mucho tiempo en el bar de Moe. Es famoso por su frase característica "¡D'oh!" cuando comete algún error o se lastima. A pesar de ser a menudo irresponsable e incompetente, se preocupa profundamente por su esposa Marge y sus tres hijos, Bart, Lisa y Maggie.</p></Col>
+          
+        </Row>
+  <br></br>
+  <br></br>
+        <Row>
+          
+          <Col md={8}><h2 class="Subtitulos"> MARGE</h2> 
+         
+          <p class="texto_Personaje">Marge Simpson es la esposa de Homer Simpson y la matriarca de la familia Simpson en la serie de televisión "Los Simpson". Es reconocida por su distintivo peinado azul en forma de torre y su personalidad compasiva y devota.
 
-            </div>
-           </div>
+Marge es una mujer cariñosa y paciente, siempre tratando de mantener la paz y la armonía en su hogar. Es una madre amorosa y dedicada a sus tres hijos, Bart, Lisa y Maggie. A menudo se preocupa por su bienestar y trata de guiarlos por el camino correcto.
+
+Marge es una ama de casa dedicada, que se esfuerza por mantener un hogar limpio y ordenado para su familia. Es una excelente cocinera y suele preparar comidas caseras para todos. A lo largo de la serie, también ha demostrado tener habilidades artísticas, como la pintura.</p> </Col>
+          <Col md={4}>  <img class="imagen3" src="https://tse4.mm.bing.net/th?id=OIP.0EDg08GCZxib5nsmMBMPCwHaFj&pid=Api&P=0&h=180" alt="The Simpsons" /> </Col>
+        </Row>
+       <br></br>
+       <br></br>
+          
+        <Row>
+          <Col md={4}>  <img class="imagen4" src="https://tse3.mm.bing.net/th?id=OIP.kXqnuO6GlyRrzJ4LUHVyYwHaE8&pid=Api&P=0&h=180" alt="The Simpsons" /> </Col>
+         
+          <Col md={8}> <h2 class="Subtitulos">LISA Y BART </h2> 
+          
+          <p class="texto_Personaje">Lisa Simpson es la hija mediana y es conocida por su inteligencia y su pasión por el aprendizaje. Es una niña superdotada, muy consciente del mundo que la rodea y con una perspectiva madura. Lisa es una talentosa saxofonista y defensora de los derechos civiles y la justicia social. A menudo se encuentra frustrada por el bajo nivel intelectual de las personas que la rodean y busca constantemente formas de mejorar el mundo.
+              Bart Simpson, por otro lado, es el hijo mayor y es famoso por su actitud traviesa y su sentido del humor irreverente. Es un bromista consumado y está constantemente metiéndose en problemas en la escuela y en la ciudad de Springfield.</p></Col>
+          
+        </Row>
+        <br></br>
+      <p class="Linea"></p>
+
+       <br></br>
+
+        
+       <h1 class="Titulos" >Episodios</h1> 
+       <br></br>
+       <br></br>
+        <Row>
+          <Col md={4}> <img class="imagen5" src="https://www.animationmagazine.net/wordpress/wp-content/uploads/simpsons-treehouse-of-horror-xxx.jpg" alt="The Simpsons" /> </Col>
+          
+          <Col md={8}>  <h2 class="Subtitulos"> The Simpsons Treehouse of Horror XXX</h2> 
+    
+          <p class="texto_Personaje"> es la trigésima entrega de la serie "Treehouse of Horror", un episodio anual con temática de Halloween de la larga serie animada de televisión "The Simpsons". Sin embargo, a partir de mi conocimiento, en septiembre de 2021, solo ha habido "Treehouse of Horror xxxi", que se emitió como parte de la 32ª temporada en 2021.
+
+Cada episodio "Treehouse of Horror" generalmente consiste en tres segmentos autónomos, parodiando varias películas de terror, ciencia ficción y otras referencias de cultura popular. Estos segmentos a menudo presentan los personajes principales de "The Simpsons" en diferentes roles y configuraciones. Son conocidos por su humor negro, comentario satírico y narración creativa.</p></Col>
+          
+        </Row>
+       <br></br>
+        <Row>
+          <Col md={4}> <img class="imagen5" src="https://1.bp.blogspot.com/-elN0qxJwyHQ/X4y65VGfcsI/AAAAAAAARIg/DjYgk-2v9Kghs50zrr-KOL-kPcMSHp8GACLcBGAsYHQ/s1350/Treehouse_of_Horror_XXXI_poster.png" alt="The Simpsons" /> </Col>
+          
+          <Col md={8}> <h2 class="Subtitulos"> The Simpsons Treehouse of Horror XXXI</h2> 
+          
+            <p class="texto_Personaje"> Es un título de episodio ficticio que has proporcionado que parece ser una combinación de "The Simpsons", la larga y animada comedia animada y sus episodios especiales anuales de Halloween llamados "Treehouse of Horror". Sin embargo, a partir de mi conocimiento, en septiembre de 2021, no ha habido un episodio titulado específicamente "Treehouse of Horror XXXL".
+
+Los episodios "Treehouse of Horror" son conocidos por sus parodias espeluznantes y cómicas de películas de terror populares, programas de televisión y literatura. Cada episodio generalmente consta de tres segmentos separados, con personajes de "The Simpsons" reinventados en varios escenarios sobrenaturales o temáticos de terror.</p></Col>
+          
+        </Row>
+      <br></br>
+        <Row>
+          <Col md={4}> <img class="imagen5" src="https://cdn.shopify.com/s/files/1/0555/1295/7111/products/SBC05_1024x1024_db10fbb3-4652-4fd2-983a-397a9331cf2a_600x.jpg?v=1616586762" alt="The Simpsons" /> </Col>
+         
+          <Col md={8}> <h2 class="Subtitulos">Treehouse of Horror </h2> 
+          
+          <p class="texto_Personaje">Los episodios de "Treehouse of Horror" son episodios especiales con temas de Halloween de "The Simpsons" que siguen un formato de tres segmentos separados, cada uno que cuenta una historia espeluznante o sobrenatural diferente. 
+          
+          Estos segmentos a menudo parodian las películas de terror populares, la ciencia ficción u otros aspectos de la cultura popular.
+
+Las tramas pueden variar ampliamente, y a menudo involucran a los personajes principales de "The Simpsons" en diferentes roles o escenarios. Los segmentos son conocidos por su humor, comentario satírico y referencias a los clásicos tropos de terror y ciencia ficción.</p> </Col>
+          
+        </Row>
+
+      </Container>
       
-          </div>
-       </div>
 
-      </div>
     );
   }
   
